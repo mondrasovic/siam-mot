997e979e915fcddb1f387cb4cbdebf39a4d7233c
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2021-09-03 14:28:28 +0200

    Add information about code profiling.

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
 @echo off
-python -m demos.demo --demo-video E:/siammot_sandbox/roundabout_01.mp4 --track-class person_vehicle --output-path E:/siammot_sandbox/tracking_vis
+python -m demos.demo --demo-video E:/interreg_sample.mp4 --track-class person_vehicle --output-path E:/siammot_sandbox/tracking_vis
---
+++ test_with_profiler.bat
@@ -0,0 +1,2 @@
+@echo off
+python -m cProfile -o inference_profiler.profile -m demos.demo --demo-video E:/interreg_sample.mp4 --track-class person_vehicle --output-path E:/siammot_sandbox/tracking_vis
