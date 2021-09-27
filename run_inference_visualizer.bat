c89017f8a4c1578462dc29e9dd171fccbf25565c
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2021-09-27 07:39:42 +0200

    Change parameters for batch files.

---
+++ run_inference_visualizer.bat
@@ -0,0 +1,3 @@
+@echo off
+
+python tools/tracking_visualizer.py E:/small_datasets/MOT17/raw_data/train/MOT17-02-DPM/img1 E:/tracking_visualization E:/projects/siam-mot/DLA-34-FPN_box_EMM/MOT17-02-DPM.json --w-scale 1.0 --h-scale 1.0
