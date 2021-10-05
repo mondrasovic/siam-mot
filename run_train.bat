7b19ff95f90c1d55ec15a832f323bac1778aae55
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2021-10-05 11:28:34 +0200

    Minor update in config. to accomodate training on UA-DETRAC dataset.

--- run_train.bat
+++ run_train.bat
@@ -1,2 +1,3 @@
 @echo off
-python -m tools.train_net --config-file ./configs/dla/DLA_34_FPN_EMM_MOT17_custom.yaml --train-dir ./demos/models/mot17_custom --model-suffix mot17_custom
+
+python -m tools.train_net --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC.yaml --train-dir ./demos/models/uadetrac --model-suffix uadetrac
