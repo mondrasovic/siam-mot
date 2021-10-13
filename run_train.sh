491f3f408622e21f54b4c6d2e162ebb839d7d8dc
mondrasovic <milan.ondrasovic@gmail.com>
2021-10-13 14:44:14 +0200

    Change config.

--- run_train.sh
+++ run_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export CUDA_AVAILABLE_DEVICES=0
+export CUDA_AVAILABLE_DEVICES=1
 
 python3 -m tools.train_net.py --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --train-dir ./demos/models/uadetrac --model-suffix uadetrac
