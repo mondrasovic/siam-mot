5548fd1efd3d9eb1083ac11f3ade91044f303c64
mondrasovic <milan.ondrasovic@gmail.com>
2021-10-14 07:46:07 +0200

    Bug fix in data filtering and debug visualizer.

--- run_debug_visualizer.bat
+++ run_debug_visualizer.bat
@@ -3,7 +3,7 @@
 setlocal EnableDelayedExpansion
 
 set "visualizer_script=tools/solver_debug_visualizer.py"
-set "data_subset=Test"
+set "data_subset=Train"
 set "dataset_dir_path=E:\datasets\UA-DETRAC_GluonCV\raw_data\Insight-MVT_Annotation_%data_subset%"
 set "out_dir_path=E:\solver_debug_visualization"
 
@@ -14,6 +14,7 @@ for %%i in (track_solver_debug_*.json) do (
     echo Processing sample !sample_name!
     set "curr_imgs_dir_path=%dataset_dir_path%\!sample_name!"
     set "curr_out_dir_path=%out_dir_path%\!sample_name!"
+    
     if exist !curr_out_dir_path!\ (
         echo Skipping !sample_name!
     ) else (
