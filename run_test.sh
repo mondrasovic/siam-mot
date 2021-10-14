617b020157a812e2356af0a56aa163de0043df1c
mondrasovic <milan.ondrasovic@gmail.com>
2021-10-14 05:01:14 +0000

    Minor update in bash script for model testing.

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python -m tools.test_net --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --model-file ./demos/models/uadetrac/DLA_34_FPN_EMM_UADETRAC_uadetrac/model_final.pth --set train --output_dir ./test_output
+export CUDA_AVAILABLE_DEVICES=1
+
+python3 -m tools.test_net --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --model-file ./demos/models/uadetrac/DLA_34_FPN_EMM_UADETRAC_uadetrac/model_final.pth --set train --output-dir ./test_output
