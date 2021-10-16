fc7b6d75b83101dfd4e7b339c3f26e92b1af543a
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2021-10-16 06:03:12 +0200

    Update configuration files.

--- run_debug_visualizer.bat
+++ run_debug_visualizer.bat
@@ -3,11 +3,11 @@
 setlocal EnableDelayedExpansion
 
 set "visualizer_script=tools/solver_debug_visualizer.py"
-set "data_subset=Train"
+set "data_subset=Test"
 set "dataset_dir_path=E:\datasets\UA-DETRAC_GluonCV\raw_data\Insight-MVT_Annotation_%data_subset%"
 set "out_dir_path=E:\solver_debug_visualization"
 
-for %%i in (track_solver_debug_*.json) do (
+for %%i in (X_track_solver_debug_*.json) do (
     echo -----------------------------------------------------------------------
     set "base_name=%%~ni"
     set "sample_name=!base_name:~-9!"
