08586f7d9b3fed1cb1fcba4559f3027f64726084
mondrasovic <milan.ondrasovic@gmail.com>
2021-11-10 12:49:13 +0000

    Minor update.

--- run_test.sh
+++ run_test.sh
@@ -2,4 +2,5 @@
 
 export CUDA_AVAILABLE_DEVICES=1
 
-python3 -m tools.test_net --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --model-file ./demos/models/uadetrac/DLA-34-FPN_box_EMM_UA_DETRAC_uadetrac/model_final.pth --test-dataset UA_DETRAC --set test --output-dir ./test_output_orig --eval-csv-file eval_results_uadetrac.csv MODEL.TRACK_HEAD.USE_REID False MODEL.TRACK_HEAD.FREEZE_DORMANT False
+python3 -m tools.test_net --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --model-file ./demos/models/uadetrac/DLA-34-FPN_box_EMM_UA_DETRAC_uadetrac/model_final.pth --test-dataset UA_DETRAC --set test --output-dir ./test_output_feature_nms --eval-csv-file eval_results_uadetrac.csv MODEL.TRACK_HEAD.USE_REID True MODEL.TRACK_HEAD.FREEZE_DORMANT False
+
