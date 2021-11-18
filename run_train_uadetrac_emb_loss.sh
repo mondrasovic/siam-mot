e7d0e1b00bc5d38ca194d3531603fe96cfdcd016
mondrasovic <milan.ondrasovic@gmail.com>
2021-11-18 15:41:32 +0000

    Setup training configuration for 'triplet loss'.

--- run_train_uadetrac_emb_loss.sh
+++ run_train_uadetrac_emb_loss.sh
@@ -2,4 +2,4 @@
 
 export CUDA_AVAILABLE_DEVICES=1
 
-python3 -m tools.train_net --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --train-dir ./demos/models/uadt_contrastive --model-suffix uadt_contrastive MODEL.DEVICE "cuda:0" MODEL.TRACK_HEAD.EMM.FEATURE_EMB_LOSS "contrastive"
+python3 -m tools.train_net --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --train-dir ./demos/models/uadt_triplet --model-suffix uadt_triplet MODEL.DEVICE "cuda:0" MODEL.TRACK_HEAD.EMM.FEATURE_EMB_LOSS "triplet"
