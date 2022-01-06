139cb90af00e8087924ef44cf19cf5d47ace23ee
mondrasovic <milan.ondrasovic@gmail.com>
2022-01-06 08:38:20 +0000

    Minor update.

--- eval_multi_hyperparam.py
+++ eval_multi_hyperparam.py
@@ -30,8 +30,11 @@ def build_model_path(train_dir_path: str, model_suffix: str) -> str:
 
 
 def build_output_dir_path(
-    output_root_path: str, dataset_name: str, model_suffix: str,
-    cfg_opts: Iterable[CfgOptSpec], cfg_val_map: Optional[Dict[str, str]] = None
+    output_root_path: str,
+    dataset_name: str,
+    model_suffix: str,
+    cfg_opts: Iterable[CfgOptSpec],
+    cfg_val_map: Optional[Dict[str, str]] = None
 ) -> str:
     if dataset_name == 'UA_DETRAC':
         dataset_shortcut = 'uadt'
@@ -54,22 +57,26 @@ def build_output_dir_path(
 
 
 def build_run_test_cmd(
-    config_file_path: str,
-    model_file_path: str,
-    dataset_name: str,
-    data_subset: str,
-    csv_file_name: str,
-    output_dir_path: str,
+    config_file_path: str, model_file_path: str, dataset_name: str,
+    data_subset: str, csv_file_name: str, output_dir_path: str,
     cfg_opts: Iterable[CfgOptSpec]
 ):
     run_test_args = [
-        'python3', '-m', 'tools.test_net',
-        '--config-file', config_file_path,
-        '--model-file', model_file_path,
-        '--test-dataset', dataset_name,
-        '--set', data_subset,
-        '--eval-csv-file', csv_file_name,
-        '--output-dir', output_dir_path,
+        'python3',
+        '-m',
+        'tools.test_net',
+        '--config-file',
+        config_file_path,
+        '--model-file',
+        model_file_path,
+        '--test-dataset',
+        dataset_name,
+        '--set',
+        data_subset,
+        '--eval-csv-file',
+        csv_file_name,
+        '--output-dir',
+        output_dir_path,
     ]
     for cfg_opt in cfg_opts:
         run_test_args.append(cfg_opt.name)
@@ -107,23 +114,28 @@ def iter_cmd_args(
 @click.command()
 @click.argument('param_json_file_path', type=click.Path(exists=True))
 @click.option(
-    '-n', '--n-out-files', type=int, default=3, show_default=True,
+    '-n',
+    '--n-out-files',
+    type=int,
+    default=3,
+    show_default=True,
     help="Number of output files."
 )
 @click.option(
-    '-f', '--file-name-format', default='run_eval_{}.sh', show_default=True,
+    '-f',
+    '--file-name-format',
+    default='run_eval_{}.sh',
+    show_default=True,
     help="Script file name format."
 )
 @click.option('--shuffle', is_flag=True, help="Randomly shuffle commands.")
 def main(
-    param_json_file_path: click.Path,
-    n_out_files: int,
-    file_name_format: str,
+    param_json_file_path: click.Path, n_out_files: int, file_name_format: str,
     shuffle: bool
 ) -> int:
     with open(param_json_file_path, 'rt') as file_handle:
         params = json.load(file_handle)
-    
+
     train_dir_path = params['train_dir_path']
     config_file_path = params['config_file_path']
     dataset_name = params['dataset_name']
@@ -134,14 +146,17 @@ def main(
     cfg_opts = [[CfgOptSpec(*c) for c in g] for g in params['cfg_opts_groups']]
     cfg_val_map = params.get('cfg_val_map')
 
-    cmds = list(iter_cmd_args(
-        train_dir_path, config_file_path, dataset_name, data_subset,
-        output_root_path, csv_file_name, model_suffixes, cfg_opts, cfg_val_map
-    ))
+    cmds = list(
+        iter_cmd_args(
+            train_dir_path, config_file_path, dataset_name, data_subset,
+            output_root_path, csv_file_name, model_suffixes, cfg_opts,
+            cfg_val_map
+        )
+    )
 
     if shuffle:
         random.shuffle(cmds)
-    
+
     print("\n\n".join(cmds))
 
     if n_out_files > 0:
@@ -152,7 +167,7 @@ def main(
         for i, (start, end) in enumerate(zip(idxs[:-1], idxs[1:]), start=1):
             with open(file_name_format.format(i), 'wt') as out_file:
                 out_file.write("\n\n".join(cmds[start:end]) + "\n")
-        
+
     return 0
 
 
---
+++ run_eval_3.sh
@@ -0,0 +1,3 @@
+python3 -m tools.test_net --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --model-file ./demos/models/uadt_orig/DLA-34-FPN_box_EMM_UA_DETRAC_uadt/model_0010000.pth --test-dataset UA_DETRAC --set test --eval-csv-file eval_results.csv --output-dir eval_orig/uadt/no_attention/0010000 MODEL.TRACK_HEAD.USE_ATTENTION False
+
+python3 -m tools.test_net --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --model-file ./demos/models/uadt_orig/DLA-34-FPN_box_EMM_UA_DETRAC_uadt/model_0060000.pth --test-dataset UA_DETRAC --set test --eval-csv-file eval_results.csv --output-dir eval_orig/uadt/no_attention/0060000 MODEL.TRACK_HEAD.USE_ATTENTION False
