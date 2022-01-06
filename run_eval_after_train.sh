8edd12ca868862a5e31c18ef467a6372f7bd98a7
mondrasovic <milan.ondrasovic@gmail.com>
2022-01-06 13:05:16 +0000

    Add script for starting evaluation after the training has finished.

---
+++ run_eval_after_train.sh
@@ -0,0 +1,14 @@
+train_process_pid=128651
+
+while ps -p $train_process_pid > /dev/null
+do
+    sleep 60
+done
+
+echo "Starting evaluation..."
+
+./run_eval_1.sh&
+sleep 1
+./run_eval_2.sh&
+sleep 1
+./run_eval_3.sh
