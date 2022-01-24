8e77c53d62476c7cd4804bd5e9d4632c72a712f6
mondrasovic <milan.ondrasovic@gmail.com>
2022-01-24 11:19:45 +0000

    change config to accomodate triplet loss

--- tools/train_net.py
+++ tools/train_net.py
@@ -17,7 +17,6 @@ from siammot.engine.trainer import do_train, do_train_old
 from siammot.modelling.rcnn import build_siammot
 from siammot.utils.get_model_name import get_model_name
 
-
 try:
     from apex import amp
 except ImportError:
@@ -25,21 +24,30 @@ except ImportError:
 
 parser = argparse.ArgumentParser(description="PyTorch SiamMOT Training")
 parser.add_argument(
-    "--config-file", default="", metavar="FILE", help="path to config file",
+    "--config-file",
+    default="",
+    metavar="FILE",
+    help="path to config file",
     type=str
 )
 parser.add_argument(
-    "--train-dir", default="",
-    help="training folder where training artifacts are dumped", type=str
+    "--train-dir",
+    default="",
+    help="training folder where training artifacts are dumped",
+    type=str
 )
 parser.add_argument(
-    "--model-suffix", default="",
-    help="model suffix to differentiate different runs", type=str
+    "--model-suffix",
+    default="",
+    help="model suffix to differentiate different runs",
+    type=str
 )
 parser.add_argument("--local_rank", type=int, default=0)
 parser.add_argument(
-    'opts', help="overwriting the training config from commandline",
-    default=None, nargs=argparse.REMAINDER
+    'opts',
+    help="overwriting the training config from commandline",
+    default=None,
+    nargs=argparse.REMAINDER
 )
 
 
@@ -77,7 +85,8 @@ def train(cfg, train_dir, local_rank, distributed, logger):
     device = torch.device(cfg.MODEL.DEVICE)
     model.to(device)
 
-    freeze_layers_if_necessary(cfg, model)
+    # TODO Activate layer freezing.
+    # freeze_layers_if_necessary(cfg, model)
 
     print("Parameters trainability status:".upper())
     for name, param in model.named_parameters():
@@ -87,48 +96,49 @@ def train(cfg, train_dir, local_rank, distributed, logger):
 
     optimizer = make_optimizer(cfg, model)
     scheduler = make_lr_scheduler(cfg, optimizer)
-    
+
     # Initialize mixed-precision training
     use_mixed_precision = cfg.DTYPE == "float16"
     amp_opt_level = 'O1' if use_mixed_precision else 'O0'
     model, optimizer = amp.initialize(model, optimizer, opt_level=amp_opt_level)
-    
+
     if distributed:
         model = torch.nn.parallel.DistributedDataParallel(
-            model, device_ids=[local_rank], output_device=local_rank,
-            broadcast_buffers=False, find_unused_parameters=True
+            model,
+            device_ids=[local_rank],
+            output_device=local_rank,
+            broadcast_buffers=False,
+            find_unused_parameters=True
         )
-    
+
     arguments = {}
     arguments["iteration"] = 0
-    
+
     save_to_disk = get_rank() == 0
     checkpointer = DetectronCheckpointer(
-        cfg, model, optimizer,
-        scheduler, train_dir, save_to_disk
+        cfg, model, optimizer, scheduler, train_dir, save_to_disk
     )
     extra_checkpoint_data = checkpointer.load(cfg.MODEL.WEIGHT)
     arguments.update(extra_checkpoint_data)
-    
+
     data_loader = build_train_data_loader(
         cfg,
         is_distributed=distributed,
         start_iter=arguments["iteration"],
     )
-    
+
     checkpoint_period = cfg.SOLVER.CHECKPOINT_PERIOD
-    
+
     tensorboard_writer = TensorboardWriter(cfg, train_dir)
-    
+
     gc.collect()
     torch.cuda.empty_cache()
-    
+
     do_train_old(
-        model, data_loader, optimizer, scheduler,
-        checkpointer, device, checkpoint_period, arguments,
-        logger, tensorboard_writer
+        model, data_loader, optimizer, scheduler, checkpointer, device,
+        checkpoint_period, arguments, logger, tensorboard_writer
     )
-    
+
     return model
 
 
@@ -137,48 +147,48 @@ def setup_env_and_logger(args, cfg):
         os.environ["WORLD_SIZE"]
     ) if "WORLD_SIZE" in os.environ else 1
     args.distributed = num_gpus > 1
-    
+
     if args.distributed:
         torch.cuda.set_device(args.local_rank)
         torch.distributed.init_process_group(
             backend="nccl", init_method="env://"
         )
         synchronize()
-    
+
     model_name = get_model_name(cfg, args.model_suffix)
     train_dir = os.path.join(args.train_dir, model_name)
     if train_dir:
         mkdir(train_dir)
-    
+
     logger = setup_logger("siammot", train_dir, get_rank())
     logger.info("Using {} GPUs".format(num_gpus))
     logger.info(args)
-    
+
     logger.info("Collecting env info (might take some time)")
     logger.info("\n" + collect_env_info())
-    
+
     logger.info("Loaded configuration file {}".format(args.config_file))
     with open(args.config_file, "r") as cf:
         config_str = "\n" + cf.read()
         logger.info(config_str)
     logger.info("Running with config:\n{}".format(cfg))
-    
+
     output_config_path = os.path.join(train_dir, 'config.yml')
     logger.info("Saving config into: {}".format(output_config_path))
     save_config(cfg, output_config_path)
-    
+
     return train_dir, logger
 
 
 def main():
     args = parser.parse_args()
-    
+
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
     cfg.freeze()
-    
+
     train_dir, logger = setup_env_and_logger(args, cfg)
-    
+
     train(cfg, train_dir, args.local_rank, args.distributed, logger)
 
 
