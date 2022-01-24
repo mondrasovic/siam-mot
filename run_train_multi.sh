74ef0197c0506b7fa45aa1358456fa1c10b573a2
mondrasovic <milan.ondrasovic@gmail.com>
2022-01-24 11:07:20 +0000

    Minor update.

--- run_train_multi.sh
+++ run_train_multi.sh
@@ -3,4 +3,4 @@
 export CUDA_AVAILABLE_DEVICES=0,1
 export WORLD_SIZE=2
 
-python3 -m torch.distributed.launch --nproc_per_node=2 tools/train_net.py --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --train-dir ./demos/models/uadetrac --model-suffix uadetrac
+python3 -m torch.distributed.launch --nproc_per_node=2 tools/train_net.py --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --train-dir ./demos/models/uadetrac_tmp --model-suffix uadetrac
