7979bd5ec29c556d8bff79749f025e8735b018a7
mondrasovic <milan.ondrasovic@gmail.com>
2022-07-29 06:00:15 +0000

    add: new configuration setting for upcoming trainings

--- run_train_uadt.sh
+++ run_train_uadt.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-python3 -m tools.train_net --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml --train-dir ./demos/models/uadt_dsa_ga --model-suffix uadt MODEL.TRACK_HEAD.ATTENTION.ENABLE "True"
+python3 -m tools.train_net \
+    --config-file ./configs/dla/DLA_34_FPN_EMM_UADETRAC_fri.yaml \
+    --train-dir ./demos/models/uadt_256prop_orig \
+    --model-suffix uadt \
+    MODEL.TRACK_HEAD.ATTENTION.ENABLE "False" \
+    MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE "256" \
+    MODEL.TRACK_HEAD.PROPOSAL_PER_IMAGE "256" \
+    SOLVER.VIDEO_CLIPS_PER_BATCH "8" \
+    GRAD_ACCUM.ENABLE "False"
